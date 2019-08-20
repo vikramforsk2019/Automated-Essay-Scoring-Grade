e372bd62002e8cddfe70a4c5b81a771931eb03c5
vikramforsk2019 <vsg16492cse2016@gmail.com>
2019-08-20 09:12:34 +0530

    up

--- essay2.py
+++ essay2.py
@@ -21,7 +21,7 @@ import re
 import nltk
 #nltk.download('stopwords')
 nltk.download('punkt')
-from nltk.corpus import stopwords
+#from nltk.corpus import stopwords
 
 essay = pd.read_csv('training_set_rel3.tsv', sep='\t', header=0,encoding="latin-1")
 
@@ -29,26 +29,6 @@ essay=essay[['essay_id','essay_set','essay','domain1_score']]
 essay=essay.dropna(axis=0)
 essay.head()
 essay.info()
-
-from nltk.stem.porter import PorterStemmer
-corpus  = []
-
-for i in range(0, 1):
-    review = re.sub('[^a-zA-Z]', ' ', essay['essay'][i])
-    review = review.lower()
-    review = review.split()
-    review = [word for word in review if not word in set(stopwords.words('english'))]
-    #ps = PorterStemmer()
-    #review = [ps.stem(word) for word in review]
-    corpus.append(review)
-    
-my_dict={}
-for i in corpus[0]:
-    if i not in my_dict:
-        my_dict[i]=1
-    else:
-        my_dict[i]+=1
-print(len(my_dict))   
 
 #tokens = nltk.word_tokenize(essay['essay'][0])
      
@@ -92,9 +72,28 @@ features_set = extract_features(essay[essay['essay_set'] == 1])
 print(features_set)
 
 
+#########vectorization by using TF-IDF###########
+#Word Frequencies with TfidfVectorizer
 
+from sklearn.feature_extraction.text import TfidfVectorizer
+def get_count_vectors(essays):
+    # create the transform
+    print(essays[0])
+    vectorizer = TfidfVectorizer(max_features = 10000,min_df=0,max_df=1000, ngram_range=(1, 3), stop_words='english')
+    # tokenize and build vocab
+    vectorizer.fit(essays)
+    feature_names = vectorizer.get_feature_names()
+    #tf_cv = count_vectors.toarray()
+    # encode document
+    vector = vectorizer.transform(essays)
+    # summarize encoded vector
+    print(vector.shape)
+    #print(vector.toarray())
+    #count_vectors = vector.toarray()
+    return feature_names,vector
 
-
+#####vectorization by bow###############
+"""
 from sklearn.feature_extraction.text import CountVectorizer
 def get_count_vectors(essays):
     
@@ -105,7 +104,7 @@ def get_count_vectors(essays):
     feature_names = vectorizer.get_feature_names()
     
     return feature_names, count_vectors
-    
+   """
 feature_names_cv, count_vectors=get_count_vectors(essay[essay['essay_set'] == 1]['essay'])       
 
 X_cv = count_vectors.toarray()
@@ -113,14 +112,13 @@ X_cv = count_vectors.toarray()
 ##############features bow+extract features concatenate#########
 import numpy as np
 from sklearn.model_selection import train_test_split
-from sklearn.metrics import mean_squared_error, r2_score
-from sklearn.model_selection import train_test_split
-from sklearn.linear_model import LinearRegression, Ridge, Lasso
-from sklearn.svm import SVR
-from sklearn import ensemble
-from sklearn.model_selection import GridSearchCV
+from sklearn.metrics import mean_squared_error
+from sklearn.linear_model import LinearRegression #, Ridge, Lasso
+#from sklearn.svm import SVR
+#from sklearn import ensemble
+#from sklearn.model_selection import GridSearchCV
 from sklearn.metrics import cohen_kappa_score
-from sklearn.linear_model import LinearRegression, Ridge, Lasso
+
 X = np.concatenate((features_set.iloc[:, 0:4].as_matrix(), X_cv), axis = 1)
 y = features_set['domain1_score'].as_matrix()
 
@@ -148,6 +146,56 @@ print('Cohen\'s kappa score: %.2f' % cohen_kappa_score(np.rint(y_pred), y_test))
 
 
 
+################svm############
+from sklearn.svm import SVC
+classifier = SVC(kernel = 'poly', random_state = 0)
+classifier.fit(X_train, y_train)
+
+# Predicting the Test set results
+labels_pred = classifier.predict(X_test)
+
+# Making the Confusion Matrix
+from sklearn.metrics import confusion_matrix
+cm = confusion_matrix(y_test, labels_pred)
+
+# Model Score
+score = classifier.score(X_test,y_test)
+
+print('Cohen\'s kappa score: %.2f' % cohen_kappa_score(np.rint(labels_pred), y_test))
+
+"""
+from sklearn.model_selection import GridSearchCV
+svr = SVC()
+
+parameters = {'kernel':['linear', 'rbf'], 'C':[1, 100], 'gamma':[0.1, 0.001]}
+
+grid = GridSearchCV(svr, parameters)
+grid.fit(X_train, y_train)
+
+y_pred = grid.predict(X_test)
+
+# summarize the results of the grid search
+print(grid.best_score_)
+print(grid.best_estimator_)
+
+# Explained variance score: 1 is perfect prediction
+print('Variance score: %.2f' % grid.score(X_test, y_test))
+
+# Cohen’s kappa score: 1 is complete agreement
+print('Cohen\'s kappa score: %.2f' % cohen_kappa_score(np.rint(y_pred), y_test))
+
+
+"""
+
+
+
+
+
+
+
+
+
+
 
 # Exploratory Data Analysis (EDA) on the data
 #import matplotlib as plot
@@ -169,86 +217,36 @@ features_set.plot.scatter(x = 'avg_word_le', y = 'domain1_score', s=10)
 
 
 
-
-
-
-
-
-
-import re 
-
-# \d is equivalent to [0-9]. 
-p = re.compile('\w+') 
-print(len(p.findall("I went to him at 11 A.M. on 4th July 1886")) )
-
-
-
-# \d+ will match a group on [0-9], group of one or greater size 
-p = re.compile('\d+') 
-print(p.findall("5I went to him at 11 A.M. on 4th July 1886")) 
-
-
-
-p = re.sub('\d') 
-print(p.findall("5I went to him at 11 A.M. on 4th July 1886")) 
-
-
-re.sub('\s','','siuta3 df 3#22 ds2')
-
-
-clean_essay = re.sub(r'\W', ' ', essay)
-
-
-
-# import the existing word and sentence tokenizing 
-# libraries 
-from nltk.tokenize import sent_tokenize, word_tokenize 
-
-text = "Natural language processing (NLP) is a field"  "of computer science, artificial intelligence"
-	
-print(len(sent_tokenize(text)) )
-#print(word_tokenize(text))` 
-
-
-
-
-
-
-from sklearn.feature_extraction.text import CountVectorizer
-
-  #bOW
-# list of text documents
-text = ["The quick brown fox jumped over the lazy dog."]
-# create the transform
-vectorizer = CountVectorizer()
-# tokenize and build vocab
-vectorizer.fit(text)
-# summarize
-print(vectorizer.vocabulary_)
-# encode document
-vector = vectorizer.transform(text)
-# summarize encoded vector
-print(vector.shape)
-print(type(vector))
-print(vector.toarray())
-
-
 #Word Frequencies with TfidfVectorizer
 	
 from sklearn.feature_extraction.text import TfidfVectorizer
-# list of text documents
+#list of text documents
 text = ["The quick brown fox jumped over the lazy dog.",
 		"The dog.",
 		"The fox"]
 # create the transform
 vectorizer = TfidfVectorizer()
 # tokenize and build vocab
-vectorizer.fit(text)
-# summarize
-print(vectorizer.vocabulary_)
-print(vectorizer.idf_)
+count_vectors = vectorizer.fit(text)
+feature_names = vectorizer.get_feature_names()
+#tf_cv = count_vectors.toarray()
 # encode document
-vector = vectorizer.transform([text[0]])
+vector = vectorizer.transform(text)
 # summarize encoded vector
 print(vector.shape)
 print(vector.toarray())
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
